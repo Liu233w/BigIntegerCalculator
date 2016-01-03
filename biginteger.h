3d2f522ca1bca9f6aa3913f28ba4d09dbdf7fec7
gu18168 <gu18168@gmail.com>
2016-01-03 16:01:29 +0800

    妈蛋的const
    全是const 要命啊

--- biginteger.h
+++ biginteger.h
@@ -12,37 +12,44 @@ class BigInteger
 {
 public:
 
-    vector<int> vec;
+	vector<int> vec;
 
-    bool negative;
+	bool negative;
 
-    BigInteger();
-    BigInteger(long int s);
-    BigInteger(string str);
-    BigInteger(const BigInteger& a);
+	// 构造函数 /////////////////////////////////
+	BigInteger();
+	BigInteger(long int s);
+	BigInteger(string str);
+	BigInteger(const BigInteger& a);
 
-    BigInteger operator = (BigInteger a);
+	// 重载运算符//////////////////////////////
+	BigInteger operator = (const BigInteger &a);
 
-    friend ostream& operator << (ostream& out, const BigInteger& a);
-    friend istream& operator >> (istream& in, BigInteger& a);
+	friend ostream& operator << (ostream& out, BigInteger& a);
+	friend istream& operator >> (istream& in, BigInteger& a);
 
-    friend BigInteger operator + (const BigInteger& a, const BigInteger& b);
-    friend BigInteger operator - (const BigInteger& a, const BigInteger& b);
-    friend BigInteger operator * (const BigInteger& a, const BigInteger& b);
-    friend BigInteger operator / (const BigInteger& a, const BigInteger& b);
+	friend BigInteger operator + (const BigInteger& a, const BigInteger& b);
+	friend BigInteger operator - (const BigInteger& a, const BigInteger& b);
+	friend BigInteger operator * (const BigInteger& a, const BigInteger& b);
+	friend BigInteger operator / (const BigInteger& a, const BigInteger& b);
 
-    friend int SizeJudge(BigInteger a, BigInteger b);
+	friend BigInteger operator += (BigInteger& a, const BigInteger& b);
+	friend BigInteger operator -= (BigInteger& a, const BigInteger& b);
 
-
+	friend int SizeJudge(BigInteger a, BigInteger b);
 };
 
-BigInteger operator + (BigInteger& a, BigInteger& b);
-BigInteger operator - (BigInteger& a, BigInteger& b);
-BigInteger operator * (BigInteger& a, BigInteger& b);
-BigInteger operator / (BigInteger& a, BigInteger& b);
+BigInteger operator + (const BigInteger& a,const BigInteger& b);
+BigInteger operator - (const BigInteger& a,const BigInteger& b);
+BigInteger operator * (const BigInteger& a,const BigInteger& b);
+BigInteger operator / (const BigInteger& a,const BigInteger& b);
 
 ostream& operator << (ostream& out, const BigInteger& a);
 istream& operator >> (istream& in, BigInteger& a);
 
+BigInteger operator += (BigInteger& a, const BigInteger& b);
+BigInteger operator -= (BigInteger& a, const BigInteger& b);
+
+int SizeJudge(BigInteger a, BigInteger b);
 
 #endif // BIGINTEGER_H
