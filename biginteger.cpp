8868e029701029cd44a978c7855f6eda542a5c6f
gu18168 <gu18168@gmail.com>
2016-01-03 16:02:51 +0800

    妈蛋const
    没话说了

--- biginteger.cpp
+++ biginteger.cpp
@@ -2,487 +2,523 @@
 
 BigInteger::BigInteger()
 {
-    vec.push_back(0);
+	vec.push_back(0);
 
-    negative = false;
+	negative = false;
 }
 
 BigInteger::BigInteger(long int s)
 {
-    if (s < 0)
-    {
-        negative = true;
+	if (s < 0)
+	{
+		negative = true;
 
-        s = -s;
-    }
-    else negative = false;
+		s = -s;
+	}
+	else negative = false;
 
-    int i;
+	int i;
 
-    while (s) //改动
-    {
-        i = s % 10;
+	if (s == 0) vec.push_back(0);
 
-        vec.push_back(i);
+	while (s)
+	{
+		i = s % 10;
 
-        s /= 10;
-    }
+		vec.push_back(i);
+
+		s /= 10;
+	}
 }
 
 BigInteger::BigInteger(string str)
 {
-    if (str[0] == '-')
-    {
-        negative = true;
+	if (str[0] == '-')
+	{
+		negative = true;
 
-        str = str.substr(1, str.size() - 1);
-    }
-    else negative = false;
+		str = str.substr(1, str.size() - 1);
+	}
+	else negative = false;
 
-    for (int i = str.size() - 1; i >= 0; i--)
-        vec.push_back(str[i] - '0');
+	for (int i = str.size() - 1; i >= 0; i--)
+		vec.push_back(str[i] - '0');
 
 }
 
 BigInteger::BigInteger(const BigInteger& a)
 {
-    vec = a.vec;
+	vec = a.vec;
 
-    negative = a.negative;
+	negative = a.negative;
 }
 
-BigInteger BigInteger::operator = (BigInteger a)
+BigInteger BigInteger::operator = (const BigInteger& a)
 {
-    vec = a.vec;
+	vec = a.vec;
 
-    negative = a.negative;
+	negative = a.negative;
 
-    return *this;
+	return *this;
 }
-
-ostream& operator << (ostream& out, const BigInteger& a)
+// 重载流 ///////////////////////////////////
+ostream& operator << (ostream& out, BigInteger& a)
 {
-    string str = "";
+	string str = "";
 
-    bool judge = false;
+	bool judge = false;
 
-    if (a.vec[a.vec.size() - 1] + '0' == '-') //判断加减的正负
-        judge = true;
+	if (a.vec[a.vec.size() - 1] + '0' == '-') //判断加减的正负
+		judge = true;
 
-    if (a.negative && !judge) //判断乘除的正负
-        str += '-';
+	if (a.negative && !judge) //判断乘除的正负
+		str += '-';
 
-    for (int i = a.vec.size() - 1; i >= 0; i--)
-    {
-        str += (a.vec[i] + '0');
-    }
+	for (int i = a.vec.size() - 1; i >= 0; i--)
+	{
+		str += (a.vec[i] + '0');
+	}
 
-    out << str;
+	out << str;
 
-    return out;
+	return out;
 }
 
 istream& operator >> (istream& in, BigInteger& a)
 {
-    string str = "";
+	string str = "";
 
-    in >> str;
+	in >> str;
 
-    a.vec.clear();
+	a.vec.clear();
 
-    for (int i = str.size() - 1; i >= 0; i--)
-    {
-        if (str[i] == '-')
-        {
-            a.negative = true;
+	for (int i = str.size() - 1; i >= 0; i--)
+	{
+		if (str[i] == '-')
+		{
+			a.negative = true;
 
-            continue;
-        }
-        a.vec.push_back(str[i] - '0');
-    }
+			continue;
+		}
+		a.vec.push_back(str[i] - '0');
+	}
 
-    return in;
+	return in;
 }
 
-BigInteger operator+(BigInteger& a, BigInteger& b)
+BigInteger operator+=(BigInteger& a, const BigInteger& b)
 {
-    BigInteger c, temp;
+	BigInteger temp;
 
-    c.vec.clear();
-    temp.vec.clear();
+	unsigned int maxt, mint, i;
 
-    bool negative_judge;
+	if (a.vec.size() > b.vec.size())
+	{
+		temp.vec = b.vec;
 
-    if (a.negative && b.negative)
-        negative_judge = true;
-    if (!a.negative && !b.negative)
-        negative_judge = false;
-    if (a.negative && !b.negative)
-    {
-        a.negative = false;
+		mint = b.vec.size();
 
-        return(b - a);
-    }
-    if (!a.negative && b.negative)
-    {
-        b.negative = false;
+		maxt = a.vec.size();
+	}
+	else
+	{
+		mint = a.vec.size();
 
-        return(a - b);
-    }
+		maxt = b.vec.size();
 
-    unsigned int i, mint, maxt;
+		temp.vec = a.vec;
 
-    if (a.vec.size() > b.vec.size())
-    {
-        c.vec = a.vec;
+		a.vec = b.vec;
+	}
 
-        temp.vec = b.vec;
+	for (i = 0; i < mint - 1; i++)
+	{
+		a.vec[i] += temp.vec[i];
 
-        mint = b.vec.size();
+		if (a.vec[i] > 9)
+		{
+			a.vec[i] -= 10;
 
-        maxt = a.vec.size();
-    }
-    else
-    {
-        c.vec = b.vec;
+			a.vec[i + 1] += 1;
+		}
+	}
 
-        temp.vec = a.vec;
+	a.vec[i] += temp.vec[i];
 
-        mint = a.vec.size();
+	if (a.vec[i] > 9)
+	{
+		a.vec[i] -= 10;
 
-        maxt = b.vec.size();
-    }
+		if (mint == maxt)
+			a.vec.push_back(1);
+		else
+			a.vec[i + 1] += 1;
+	}
 
-    for (i = 0; i < mint - 1; i++)
-    {
-        c.vec[i] += temp.vec[i];
+	for (i = mint - 1; i < maxt - 1; i++) //减1怕相等的时候越界
+	{
+		if (a.vec[i] > 9)
+		{
+			a.vec[i] -= 10;
 
-        if (c.vec[i] > 9)
-        {
-            c.vec[i] -= 10;
+			a.vec[i + 1] += 1;
+		}
+	}
 
-            c.vec[i + 1] += 1;
-        }
-    }
+	if (a.vec[i] > 9)
+	{
+		a.vec[i] -= 10;
 
-    c.vec[i] += temp.vec[i];
+		a.vec.push_back(1);
+	}
 
-    if (c.vec[i] > 9)
-    {
-        c.vec[i] -= 10;
+	i = a.vec.size() - 1;
 
-        if (mint == maxt)
-            c.vec.push_back(1);
-        else
-            c.vec[i + 1] += 1;
-    }
+	while (a.vec[i] == 0)
+	{
+		a.vec.pop_back();
 
-    for (i = mint; i < maxt - 1;i++)
-    {
-        if (c.vec[i] > 9)
-        {
-            c.vec[i] -= 10;
+		i--;
+	}
+	return a;
+}
+// += 检查完成 ///////////////////////////////
+BigInteger operator-=(BigInteger& a, const BigInteger& b)
+{
+	bool judge;
 
-            c.vec[i + 1] += 1;
-        }
-    }
+	unsigned int i, mint, maxt;
 
-    if (c.vec[i] > 9)
-    {
-        c.vec[i] -= 10;
+	BigInteger temp, zero(0);
 
-        c.vec.push_back(1);
-    }
+	temp.vec.clear();
 
-    i = c.vec.size() - 1;
+	if (a.vec.size() > b.vec.size())
+	{
+		mint = b.vec.size();
 
-    while (c.vec[i] == 0)
-    {
-        c.vec.pop_back();
+		maxt = a.vec.size();
 
-        i--;
-    }
+		temp.vec = b.vec;
 
-    if (negative_judge)
-        c.vec.push_back('-' - '0');
+		judge = false;
+	}
+	else if (a.vec.size() == b.vec.size())
+	{
+		for (i = a.vec.size() - 1; i >= 1; i--)
+		{
+			if (a.vec[i] < b.vec[i])
+			{
+				mint = a.vec.size();
 
-    return c;
+				maxt = b.vec.size();
+
+				temp.vec = a.vec;
+
+				a.vec = b.vec;
+
+				judge = true;
+
+				break;
+			}
+			else if (a.vec[i] > b.vec[i])
+			{
+				mint = b.vec.size();
+
+				maxt = a.vec.size();
+
+				temp.vec = b.vec;
+
+				judge = false;
+
+				break;
+			}
+		}
+		if (i == 0 && a.vec[0] == b.vec[0])
+		{
+			a.vec.clear();
+
+			a.vec.push_back(0);
+
+			return a;
+		}
+	}
+	else
+	{
+		mint = a.vec.size();
+
+		maxt = b.vec.size();
+
+		temp.vec = a.vec;
+
+		a.vec = b.vec;
+
+		judge = true;
+	}
+
+
+	for (i = 0; i < mint; i++)
+	{
+		a.vec[i] -= temp.vec[i];
+
+		if (a.vec[i] < 0)
+		{
+			a.vec[i] += 10;
+
+			a.vec[i + 1] -= 1;
+		}
+	}
+
+	if (maxt > mint)
+		for (i = mint; i < maxt; i++)
+		{
+			if (a.vec[i] < 0)
+			{
+				a.vec[i] += 10;
+
+				a.vec[i + 1] -= 1;
+			}
+		}
+
+	i = a.vec.size() - 1;
+
+	while (a.vec[i] == 0)
+	{
+		a.vec.pop_back();
+
+		i--;
+	}
+
+	if (judge)
+		a.vec.push_back('-' - '0');
+
+	return a;
+}
+// -=检查完毕 /////////////////////////////////
+
+BigInteger operator+(const BigInteger& a, const BigInteger& b)
+{
+	BigInteger temp1, temp2;
+
+	temp1.vec.clear();
+	temp2.vec.clear();
+
+	temp1 = a;
+	temp2 = b;
+
+	if (a.negative && b.negative)
+	{
+		temp1.negative = false;
+		temp2.negative = false;
+
+		temp1 += temp2;
+
+		temp1.vec.push_back('-' - '0');
+	}
+	else if (a.negative && !b.negative)
+	{
+		temp1.negative = false;
+
+		temp2 -= temp1;
+
+		temp1 = temp2;
+	}
+	else if (!a.negative && b.negative)
+	{
+		temp2.negative = false;
+
+		temp1 -= temp2;
+	}
+	else temp1 += temp2;
+
+	return temp1;
+}
+// +检查完毕 ///////////////////////////////////////
+
+BigInteger operator - (const BigInteger& a, const BigInteger& b)
+{
+
+	BigInteger temp1, temp2;
+
+	temp1.vec.clear();
+	temp2.vec.clear();
+
+	temp1 = a;
+	temp2 = b;
+
+	if (a.negative && b.negative)
+	{
+		temp1.negative = false;
+		temp2.negative = false;
+
+		temp2 -= temp1;
+
+		temp1 = temp2;
+	}
+	else if (a.negative && !b.negative)
+	{
+		temp1.negative = false;
+
+		temp1 += temp2;
+
+		temp1.vec.push_back('-' - '0');
+	}
+	else if (!a.negative && b.negative)
+	{
+		temp2.negative = false;
+
+		temp1 += temp2;
+	}
+	else temp1 -= temp2;
+
+	return temp1;
+}
+// -检查完毕 /////////////////////////////////////
+BigInteger operator * (const BigInteger& a, const BigInteger& b)
+{
+	BigInteger c;
+
+	c.vec.clear();
+
+	unsigned int i, j, temp;
+
+	for (i = 0; i < (a.vec.size() + b.vec.size()); i++)
+		c.vec.push_back(0);
+
+	for (i = 0; i < a.vec.size(); i++)
+	{
+		for (j = 0; j < b.vec.size(); j++)
+		{
+			temp = c.vec[i + j] + a.vec[i] * b.vec[j];
+
+			if (temp > 9)
+			{
+				if ((i + j + 1)<(a.vec.size() + b.vec.size()))
+				{
+					c.vec[i + j + 1] += temp / 10;
+
+					temp %= 10;
+				}
+				else
+				{
+					c.vec.push_back(temp / 10);
+
+					temp %= 10;
+				}
+			}
+			c.vec[i + j] = temp;
+		}
+	}
+	i = c.vec.size() - 1;
+
+	while (c.vec[i] == 0)
+	{
+		c.vec.pop_back();
+
+		i--;
+	}
+
+	if ((a.negative && !b.negative) || (!a.negative && b.negative))
+	{
+		c.vec.push_back('-' - '0');
+
+		c.negative = true;
+	}
+	else
+		c.negative = false;
+
+	return c;
+}
+// *检查完毕 ////////////////////////////////////
+
+BigInteger operator / (const BigInteger& a, const BigInteger& b)
+{
+	int i, j = 1;
+
+	if (SizeJudge(a, b) == -1)
+	{
+		return (BigInteger)0;
+	}
+	else if (SizeJudge(a, b) == 0)
+	{
+		return (BigInteger)1;
+	}
+
+	if (b.vec.size() == 1 && b.vec[0] == 0)
+	{
+		BigInteger err("error");
+
+		return err;
+	}
+
+	i = a.vec.size() - b.vec.size();
+
+	while (i)
+	{
+		j *= 10;
+
+		i--;
+	}
+
+	BigInteger ans(j), temp, ok(1);
+
+	temp = ans*b;
+
+	if (SizeJudge(a, temp) == 1)
+		while (SizeJudge(a, temp) == 1)
+		{
+			ans = ans + ok;
+
+			temp = ans*b;
+		}
+	else if (SizeJudge(a, temp) == -1)
+		while (SizeJudge(a, temp) == -1)
+		{
+			ans = ans - ok;
+
+			temp = ans*b;
+		}
+
+	if ((a.negative && !b.negative) || (!a.negative && b.negative))
+	{
+		char sign = '-';
+
+		ans.vec.push_back(sign - '0');
+
+		ans.negative = 1;
+	}
+	else
+		ans.negative = 0;
+
+	return ans;
 }
 
-BigInteger operator - (BigInteger& a, BigInteger& b)
+int SizeJudge(const BigInteger a, const BigInteger b)
 {
-    if (a.negative && b.negative)
-    {
-        a.negative = false;
-        b.negative = false;
-
-        return (b - a);
-    }
-    if (a.negative && !b.negative)
-    {
-        b.negative = true;
-
-        return (a + b);
-    }
-    if (!a.negative && b.negative)
-    {
-        b.negative = false;
-
-        return (a + b);
-    }
-
-    BigInteger c, temp;
-
-    bool judge;
-
-    c.vec.clear();
-    temp.vec.clear();
-
-    unsigned int i, mint, maxt;
-
-    if (a.vec.size() > b.vec.size())
-    {
-        c.vec = a.vec;
-
-        temp.vec = b.vec;
-
-        mint = b.vec.size();
-
-        maxt = a.vec.size();
-
-        judge = false;
-    }
-    else if (a.vec.size() == b.vec.size())
-    {
-        for (i = a.vec.size() - 1; i >= 0; i--)
-        {
-            if (a.vec[i] < b.vec[i])
-            {
-                c.vec = b.vec;
-
-                temp.vec = a.vec;
-
-                mint = a.vec.size();
-
-                maxt = b.vec.size();
-
-                judge = true;
-
-                break;
-            }
-            else if (a.vec[i] > b.vec[i])
-            {
-                c.vec = a.vec;
-
-                temp.vec = b.vec;
-
-                mint = b.vec.size();
-
-                maxt = a.vec.size();
-
-                judge = false;
-
-                break;
-            }
-        }
-
-        if (i == -1) return (BigInteger)0;
-    }
-    else
-    {
-        c.vec = b.vec;
-
-        temp.vec = a.vec;
-
-        mint = a.vec.size();
-
-        maxt = b.vec.size();
-
-        judge = true;
-    }
-
-
-    for (i = 0; i < mint; i++)
-    {
-        c.vec[i] -= temp.vec[i];
-
-        if (c.vec[i] < 0)
-        {
-            c.vec[i] += 10;
-
-            c.vec[i + 1] -= 1;
-        }
-    }
-
-    if (maxt > mint)
-        for (i = mint; i < maxt; i++)
-        {
-            if (c.vec[i] < 0)
-            {
-                c.vec[i] += 10;
-
-                c.vec[i + 1] -= 1;
-            }
-        }
-
-    if (judge)
-        c.vec.push_back('-' - '0');
-
-    i = c.vec.size() - 1;
-
-    while (c.vec[i] == 0)
-    {
-        c.vec.pop_back();
-
-        i--;
-    }
-
-    return c;
-}
-
-BigInteger operator * (BigInteger& a, BigInteger& b)
-{
-    BigInteger c;
-
-    unsigned int i, j, temp;
-
-    for (i = 0;i < (a.vec.size() + b.vec.size()); i++)
-        c.vec.push_back(0);
-
-    for (i = 0; i < a.vec.size(); i++)
-    {
-        for (j = 0; j < b.vec.size(); j++)
-        {
-            temp = c.vec[i + j] + a.vec[i] * b.vec[j];
-
-            if (temp > 9)
-            {
-                if ((i + j + 1) < (a.vec.size() + b.vec.size()))
-                {
-                    c.vec[i + j + 1] += temp / 10;
-
-                    temp %= 10;
-                }
-                else
-                {
-                    c.vec.push_back(temp / 10);
-
-                    temp %= 10;
-                }
-            }
-            c.vec[i + j] = temp;
-        }
-    }
-    i = c.vec.size() - 1;
-
-    while (c.vec[i] == 0)
-    {
-        c.vec.pop_back();
-
-        i--;
-    }
-
-    if ((a.negative && !b.negative) || (!a.negative && b.negative))
-    {
-        c.vec.push_back('-' - '0');
-
-        c.negative = true;
-    }
-    else
-        c.negative = false;
-
-    return c;
-}
-
-BigInteger operator / (BigInteger& a, BigInteger& b)
-{
-    int i, j = 1;
-
-    if (SizeJudge(a, b) == -1)
-    {
-        return (BigInteger)0;
-    }
-    else if (SizeJudge(a, b) == 0)
-    {
-        return (BigInteger)1;
-    }
-
-    if (b.vec.size() == 1 && b.vec[0] == 0)
-    {
-        BigInteger err("error");
-
-        return err;
-    }
-
-    i = a.vec.size() - b.vec.size();
-
-    while (i)
-    {
-        j *= 10;
-
-        i--;
-    }
-
-    BigInteger ans(j), temp, ok(1);
-
-    temp = ans*b;
-
-    if (SizeJudge(a, temp) == 1)
-        while (SizeJudge(a, temp) == 1)
-        {
-            ans = ans + ok;
-
-            temp = ans*b;
-        }
-    else if (SizeJudge(a, temp) == -1)
-        while (SizeJudge(a, temp) == -1)
-        {
-            ans = ans - ok;
-
-            temp = ans*b;
-        }
-
-    if ((a.negative && !b.negative) || (!a.negative && b.negative))
-    {
-        char sign = '-';
-
-        ans.vec.push_back(sign - '0');
-
-        ans.negative = 1;
-    }
-    else
-        ans.negative = 0;
-
-    return ans;
-}
-
-int SizeJudge(BigInteger a, BigInteger b)
-{
-    int alen, blen, i;
-    alen = a.vec.size();
-    blen = b.vec.size();
-    if (alen > blen)
-    {
-        return 1;
-    }
-    else if (alen == blen)
-    {
-        for (i = alen - 1;i >= 0;i--)
-        {
-            if (a.vec[i] > b.vec[i])
-            {
-                return 1;
-            }
-            else if (a.vec[i] < b.vec[i])
-            {
-                return -1;
-            }
-        }
-        if (i == 0)
-            return 0;
-    }
-    else
-    {
-        return -1;
-    }
+	int alen, blen, i;
+	alen = a.vec.size();
+	blen = b.vec.size();
+
+	if (alen>blen)
+	{
+		return 1;
+	}
+	else if (alen == blen)
+	{
+		for (i = alen - 1; i >= 0; i--)
+		{
+			if (a.vec[i]>b.vec[i])
+			{
+				return 1;
+			}
+			else if (a.vec[i]<b.vec[i])
+			{
+				return -1;
+			}
+		}
+		if (i == 0)
+			return 0;
+	}
+	else
+		return -1;
 }
